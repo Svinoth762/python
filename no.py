02d1b8ea5297e26364136b9ffc5ca6f84b9ee8c5
Svinoth762 <120073459+Svinoth762@users.noreply.github.com>
2023-01-10 11:40:30 +0530

    Add files via upload

---
+++ no.py
@@ -0,0 +1,9 @@
+n=input("Enter the any:")
+if n==n[ : :-1]:
+    n=n
+    print("True")
+    print(n,"reads as",n[ : :-1],"from left to right and from right to left.So it is a palindome")
+else:
+    print("False")
+    print("From right to left it reads",n,".From left to right it reads",n[ : :-1],"Therfore it is not a palindome")
+  
